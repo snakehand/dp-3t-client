3f617e409f3f03ceb90eff087fab999b88e896db
Frank Andrew Stevenson <frank.stevenson@magseis.com>
2020-04-06 11:53:36 +0200

    Add replay mechanism to verify matches of captured ephemerals

--- src/lib.rs
+++ src/lib.rs
@@ -4,13 +4,12 @@ pub mod session;
 
 #[cfg(test)]
 mod tests {
-    use crate::session::Session;
+    use crate::session::{ReplayKey, Session};
     use std::path::PathBuf;
 
     #[test]
     fn test_session() {
-        // let session = crate::session::Session::new();
-        let session = Session::new();
+        let mut session = Session::new();
         let path = PathBuf::from("test_session.json");
         assert!(session.save(&path).is_ok());
         let s2 = Session::load(&path).unwrap();
@@ -27,5 +26,30 @@ mod tests {
         assert_ne!(eph1, eph2);
         session.get_ephemeral(&mut eph1, 1).unwrap();
         assert_eq!(eph1, eph2);
+    }
+
+    #[test]
+    fn test_rewind() {
+        let mut session = Session::new();
+        let mut eph = [0u8; 16];
+        session.get_ephemeral(&mut eph, 0).unwrap();
+        session.set_future(20); // 20 days into future
+        session.get_ephemeral(&mut eph, 0).unwrap();
+        session.set_future(0); // rewind
+                               // This will fail as key from day 0 is no longer available
+        assert!(session.get_ephemeral(&mut eph, 0).is_err());
+    }
+
+    #[test]
+    fn test_recovery() {
+        let mut session = Session::new();
+        let mut eph = [0u8; 16];
+        session.set_future(10);
+        session.get_ephemeral(&mut eph, 3).unwrap();
+        session.set_future(0);
+        let (day, secret) = session.get_secret().unwrap();
+        let mut rplay = ReplayKey::new(day, day + 14, 8, &secret);
+        let found = rplay.any(|e| e.token == eph);
+        assert!(found);
     }
 }
--- src/session.rs
+++ src/session.rs
@@ -6,8 +6,11 @@ use ring::{digest, hmac};
 use serde::{Deserialize, Serialize};
 use std::fs::File;
 use std::io::prelude::*;
+use std::iter::Iterator;
 use std::path::PathBuf;
 use std::time::SystemTime;
+
+const MAX_HISTORY: u32 = 14;
 
 #[derive(Serialize, Deserialize, Debug, PartialEq, Eq, PartialOrd, Ord, Copy, Clone)]
 struct SessionKey {
@@ -40,6 +43,8 @@ impl SessionKey {
 #[derive(Debug, PartialEq, Eq)]
 pub struct Session {
     recent_keys: Vec<SessionKey>,
+    test_future: u32,
+    path: Option<PathBuf>,
 }
 
 impl Session {
@@ -58,7 +63,11 @@ impl Session {
             Err(_) => return Err("Could not decode file"),
         };
         recent_keys.sort();
-        Ok(Session { recent_keys })
+        Ok(Session {
+            recent_keys,
+            test_future: 0,
+            path: Some(path.to_owned()),
+        })
     }
 
     pub fn new() -> Session {
@@ -72,10 +81,14 @@ impl Session {
         let mut recent_keys = Vec::new();
         recent_keys.push(SessionKey { julian_day, key });
         // recent_keys.push( recent_keys[0].next() );
-        Session { recent_keys }
+        Session {
+            recent_keys,
+            test_future: 0,
+            path: None,
+        }
     }
 
-    pub fn save(&self, path: &PathBuf) -> Result<(), &'static str> {
+    pub fn save(&mut self, path: &PathBuf) -> Result<(), &'static str> {
         let mut file = match File::create(path) {
             Ok(f) => f,
             Err(_) => return Err("Could not create file"),
@@ -85,7 +98,10 @@ impl Session {
             Err(_) => return Err("Could not serialise"),
         };
         match file.write_all(serialized.as_bytes()) {
-            Ok(_) => Ok(()),
+            Ok(_) => {
+                self.path = Some(path.to_owned());
+                Ok(())
+            }
             Err(_) => Err("Could not write to file"),
         }
     }
@@ -97,7 +113,7 @@ impl Session {
         let now = SystemTime::now()
             .duration_since(SystemTime::UNIX_EPOCH)
             .unwrap();
-        let julian_day = ((now.as_secs() / (24 * 3600)) + 2_440_588) as u32;
+        let julian_day = ((now.as_secs() / (24 * 3600)) + 2_440_588) as u32 + self.test_future;
         if julian_day < self.recent_keys[0].julian_day {
             return Err("No keys from past available");
         }
@@ -125,8 +141,78 @@ impl Session {
                 key.get_ephemeral(dst, index);
             }
         }
+        self.recent_keys
+            .retain(|k| k.julian_day + MAX_HISTORY >= julian_day);
         self.recent_keys.sort();
-        // TODO: Prune old keys - save keys
+        if let Some(path) = self.path.take() {
+            let _ = self.save(&path);
+        }
         Ok(())
+    }
+
+    pub fn set_future(&mut self, future: u32) {
+        self.test_future = future;
+    }
+
+    pub fn get_secret(&self) -> Option<(u32, [u8; 32])> {
+        let now = SystemTime::now()
+            .duration_since(SystemTime::UNIX_EPOCH)
+            .unwrap();
+        let julian_day = ((now.as_secs() / (24 * 3600)) + 2_440_588) as u32 + self.test_future;
+        for k in &self.recent_keys {
+            if k.julian_day + MAX_HISTORY >= julian_day {
+                return Some((k.julian_day, k.key));
+            }
+        }
+        None
+    }
+}
+
+pub struct ReplayKey {
+    key: SessionKey,
+    end_day: u32,
+    current_index: u32,
+    max_index: u32,
+}
+
+#[derive(Debug)]
+pub struct Ephemeral {
+    pub day: u32,
+    pub token: [u8; 16],
+}
+
+impl ReplayKey {
+    pub fn new(start_day: u32, end_day: u32, max_index: u32, key: &[u8; 32]) -> ReplayKey {
+        let key = SessionKey {
+            julian_day: start_day,
+            key: *key,
+        };
+        ReplayKey {
+            key,
+            end_day,
+            current_index: 0,
+            max_index,
+        }
+    }
+}
+
+impl Iterator for ReplayKey {
+    type Item = Ephemeral;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        if self.current_index >= self.max_index {
+            self.current_index = 0;
+            self.key = self.key.next();
+        }
+        if self.key.julian_day > self.end_day {
+            return None;
+        }
+        let mut eph = Ephemeral {
+            day: self.key.julian_day,
+            token: [0; 16],
+        };
+        self.key.get_ephemeral(&mut eph.token, self.current_index);
+        self.current_index += 1;
+        Some(eph)
     }
 }
